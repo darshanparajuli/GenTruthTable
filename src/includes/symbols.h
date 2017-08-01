2aa576a90b79cfbaaab11c19f43f23a454354406
Darshan Parajuli <parajulidarshan@gmail.com>
2017-07-31 22:24:35 -0700

    Make symbols const

--- src/includes/symbols.h
+++ src/includes/symbols.h
@@ -5,16 +5,16 @@
 
 namespace symbol
 {
-    std::string NOT = "~";
-    std::string AND = "*";
-    std::string OR = "+";
-    std::string XOR = "<+>";
-    std::string IMPLICATION = "=>";
-    std::string EQUIVALENCE = "<>";
-    std::string LEFT_PAREN = "(";
-    std::string RIGHT_PAREN = ")";
-    std::string TRUE = "1";
-    std::string FALSE = "0";
+    const std::string NOT = "~";
+    const std::string AND = "*";
+    const std::string OR = "+";
+    const std::string XOR = "<+>";
+    const std::string IMPLICATION = "=>";
+    const std::string EQUIVALENCE = "<>";
+    const std::string LEFT_PAREN = "(";
+    const std::string RIGHT_PAREN = ")";
+    const std::string TRUE = "1";
+    const std::string FALSE = "0";
 }
 
 #endif
